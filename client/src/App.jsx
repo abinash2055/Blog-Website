16b2aa5944d2107517fbe14e420cf752e1be3b43
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2025-10-22 10:46:45 +0545

    Blog App tailwindcss added

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-orange-600">
+        Tailwind CSS + React + Vite ✅
+      </h1>
+    </>
+  );
+}
+
+export default App;
