97072f76a0310733da1ab21759289091ba95d634
kusl007 <kushal.bansal.1001@gmail.com>
2025-11-14 21:27:36 +0530

    changed cors

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL,"https://blog-website-trail.vercel.app/"],
     credentials: true,
   })
 );
